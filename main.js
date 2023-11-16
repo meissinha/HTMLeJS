09a43884891c2d5ba5393507803bcede36b82142
Mei Tanamati <108132708+meissinha@users.noreply.github.com>
2023-11-16 20:14:48 -0300

    Update main.js
    aaaa

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementosAudio) {
+    document.querySelector(idElementosAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll(' .tecla');
+
+//para
+
+for (let contador = 0; contador <listaDeTeclas.length;contador++) {
+
+const tecla = listaDeTeclas[contador];
+
+const instrumento = tecla.classeList[1];
+
+const idAudio = `#som_${instrumento}`;//template string
+
+tecla.onlick = function () {
+tocaSom(idAudio);
+}
+
+tecla.onkeydown = function (evento){
+
+    console.log(evento.cod)
+
+    if(evento.code = `Space`){
+        tecla.classList.add(`ativa`);
+    }
+
+    if (evento.code ==='Enter') {
+tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyyup = function () {
+        tecla.classList.remove(`ativa`);
+
+    }
+}
+
+}
