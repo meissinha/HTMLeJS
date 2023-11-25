31308fd1fac51fb00faa257ea3c7308d6bd2bfd3
Mei Tanamati <108132708+meissinha@users.noreply.github.com>
2023-11-24 22:29:07 -0300

    Update main.js
    ggggg

--- main.js
+++ main.js
@@ -9,31 +9,20 @@ const listaDeTeclas = document.querySelectorAll(' .tecla');
 for (let contador = 0; contador <listaDeTeclas.length;contador++) {
 
 const tecla = listaDeTeclas[contador];
-
 const instrumento = tecla.classeList[1];
-
-const idAudio = `#som_${instrumento}`;//template string
+const idAudio = '#som_${instrumento}';//template string
 
 tecla.onlick = function () {
 tocaSom(idAudio);
 }
 
 tecla.onkeydown = function (evento){
+    console.log(evento.code)
 
-    console.log(evento.cod)
-
-    if(evento.code = `Space`){
-        tecla.classList.add(`ativa`);
-    }
-
-    if (evento.code ==='Enter') {
-tecla.classList.add('ativa');
-    }
-
-    tecla.onkeyyup = function () {
-        tecla.classList.remove(`ativa`);
-
+    if (evento.code = 'Space') {
+        tecla.classList.add('ativa');
     }
 }
 
-}}
+
+tecla.onkeyup = function ()
